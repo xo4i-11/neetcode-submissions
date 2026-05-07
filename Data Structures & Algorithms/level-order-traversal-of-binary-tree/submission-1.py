f125487ad98f4c9c8b3aa878dfadccf3a91638c9
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-05-07 04:02:43 -0400

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        
+        queue = deque()
+        queue.append(root)
+
+        res=[]
+
+
+        while queue:
+            #track the number of node in that level
+            node_count= len(queue)
+            level=[]
+            for i in range (node_count):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            res.append(level)
+        
+        return res
+
+            
+
+
+        
+
+            
+        
+
+
+"""
+ideas: binary tree level => use bfs:
+    when we do bfs:
+        - in order to track the node in the same level: 
+            + we remove node in same level 1 by 1, then add their children to the queue
+            => track the length of the queue, then remove the exact number of node 
+            from queue to ensure the node in the same level
+            Basically, the number of node we remove from queue in 1 loop = num of node in that level
+
+
+
+
+
+"""
+        
