0bff134a2a832e89835942c15cbd6247494dd0d5
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-05-31 05:46:35 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,36 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        #min heap with k largest int
+            self.min_heap = nums 
+            self.k = k
+
+            heapq.heapify(self.min_heap)
+            while len(self.min_heap) > k:
+                heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+
+        return self.min_heap[0]        
+
+
+
+"""
+problem:
+    + Design a class to find the kth largest int in a stream of values, include duplicate
+    
+- constructor: init the object, given an int k and stream int nums
+- add: add elem to the stream, return the kth largest elem
+
+idea: using min heap
+
+- min heap: + add (push) elem in log(n) time
+            + pop elem in log(n) time
+            + get min uin O(1) time
+
+- use min heap because when we add el
+"""
