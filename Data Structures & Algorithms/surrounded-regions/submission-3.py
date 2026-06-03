59c11bdbbb8687c2bd7ff17e3394a4b79c8b8150
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-06-02 22:32:11 -0400

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,68 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        visited= set()
+
+        rows = len(board)
+        cols = len(board[0])
+        
+        #dfs used to traverse
+        def dfs(r,c):
+            if r<0 or r>=rows or c<0 or c>=cols:
+                return 
+
+            if board[r][c] == "X" or (r,c) in visited:
+                return 
+            
+
+            visited.add((r,c))
+
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+            return 
+
+
+        #start DFS from the border O
+        for r in range(rows):
+            #left border
+            if board[r][0] == "O":
+                dfs(r,0)
+            #right border
+            if board[r][cols-1] == "O":
+                dfs(r,cols-1)
+        
+        for c in range(cols):
+            #up boarder
+            if board[0][c] == "O":
+                dfs(0,c)
+            #down boarder
+            if board[rows-1][c] == "O":
+                dfs(rows-1,c)
+        
+        #for all of every other O that is not from border
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O" and (r,c) not in visited:
+                    board[r][c] = "X"
+
+        return 
+
+            
+
+"""
+Idea: we do DFS
+
+- create a visted set to track all of the visited node
+- traverse all border cell:
+    + when a border cell is O, do DFS and mark all of the O neighbor as visited
+- after all border-connected region are found:
+    + if a cell is O and not visited => it must be surrounded => change to X
+    + otherwise, leave it the same
+
+"""
+
+
+
+        
