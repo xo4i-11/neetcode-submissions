2c8578e9c9645d050dd264b7d344d891f39fef6c
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-06-12 12:53:01 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,41 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs():
+            # stopping case
+            if len(subset) == len(nums):
+                res.append(subset.copy())
+                return 
+            
+            #loop through every node except itself
+            for num in nums:
+                if num not in subset:
+                    subset.append(num)
+                    dfs()
+
+                    subset.pop()
+            
+        
+        dfs()
+        return res
+
+
+
+
+"""
+problem:
+    - find all the permutation
+
+idea:
+    - we use dfs + backtracking 
+    - we only add to the res only if the len(susbet) == len(nums)
+    
+    - when traversing, we check that number + include all the neighbor (using a loop)
+
+
+
+"""
+        
