3846987966f7db2cbccf81239c5afbd6004492d7
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-06-23 04:50:27 -0400

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+
+        hashmap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        curr_str = ""
+
+        def dfs(i):
+            nonlocal curr_str 
+            if i == len(digits):
+                res.append(curr_str)
+                return 
+            
+            for c in hashmap[digits[i]]:
+                curr_str+=c
+                dfs(i+1)
+                curr_str = curr_str[0:-1]
+        
+        if digits:
+            dfs(0)
+        
+        return res
