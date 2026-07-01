3d80441746c632cde20d65af5988cb9e43cd3c1e
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-07-01 09:49:28 -0400

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # create graph amd indegree
+        graph = defaultdict(list)
+        indegree = [0]*numCourses
+
+        for a,b in prerequisites:
+            graph[b].append(a)
+            indegree[a]+=1
+        
+        #doing kahn algo -> add all vertex with indegree =0
+        queue = deque()
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        finish = 0
+        output = []
+
+        while queue:
+            node = queue.popleft()
+            output.append(node)
+            finish +=1
+
+            for neigh in graph[node]:
+                indegree[neigh]-=1
+                if indegree[neigh]==0:
+                    queue.append(neigh)
+
+        
+        if finish != numCourses:
+            return []
+        else:
+            return output
+
+
+
+
