75505057769b53449221f784a2e4c5adefe422f8
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-07-27 17:01:34 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,53 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keyStore={} #key =string, value =[list of [value (string),timestamp (int)]]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key]=[]
+        self.keyStore[key].append([value,timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        # 1. find all the values in that key
+        if key in self.keyStore:
+            values=self.keyStore[key]
+        else:
+            values=[]
+        
+        # 2. binary search (doable cuz the timstamp is in sorted order)
+        #create to track the latest string
+        res=""
+
+        l=0
+        r=len(values)-1
+
+        while l<=r:
+            m = (l+r)//2
+            if values[m][1] <= timestamp:
+                res=values[m][0]
+                l= m+1
+            else:
+                r= m-1
+        return res
+
+
+#IDEAS:
+#for get() function:
+#   values = [
+#      
+#       ["a", 1],
+#       ["b", 4],
+#       ["c", 6],
+#       ["d", 9]
+#       
+#       ]
+
+# we wanna get('foo',7)
+# => it should return c
+
+
+
+
+
