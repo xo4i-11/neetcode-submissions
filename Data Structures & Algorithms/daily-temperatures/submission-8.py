e69753ee79e98e9a6eea6c300e153cbe575adefb
Duy Nghiem <nghiemanhduy1308@gmail.com>
2026-07-27 00:30:35 -0400

    Add: daily-temperatures - submission-8

---
+++ Data Structures & Algorithms/daily-temperatures/submission-8.py
@@ -0,0 +1,51 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        if len(temperatures) == 0:
+            return []
+
+        res = [0] * len(temperatures)
+        stack = []
+
+
+        for i in range(len(temperatures)):
+            
+            while len(stack) != 0 and temperatures[i] > temperatures[stack[-1]]:
+                latest_day = stack.pop()
+                days = i - latest_day
+                res[latest_day] = days
+
+            stack.append(i)
+
+        return res          
+
+
+
+
+
+
+
+
+
+"""
+problem: 
+    - given a list of temperature
+    - output: a list of number of days that temperature is higher than that day
+
+idea:
+
+TLDR: USE STACK TO STORE THE INDEX OF THE DAY THAT HAVEN;T FOUND A WARMER DAY
+
+1. Create a result list filled with zeros.
+2. Use a stack to store pairs of (temperature, index) for days that haven't found a warmer day yet.
+3. Iterate through the temperature list:
+    - While the stack is not empty and the current temperature is warmer than the top of the stack:
+        + Pop the top element.
+        + Compute how many days passed and update the result.
+    - Push the current day onto the stack.
+    
+4. Return the filled result list.
+
+
+
+"""
+        
